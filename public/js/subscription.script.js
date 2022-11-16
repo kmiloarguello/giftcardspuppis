6e5e4275b9ff53ccd445eff1b3f41e9bd990f667
Camillo Arguello <camilo.arguello.rincon@gmail.com>
2022-11-16 19:33:00 +0100

    [CA] Updates in the subscription documentation

--- public/js/subscription.script.js
+++ public/js/subscription.script.js
@@ -20,10 +20,7 @@ const Subscriptions = {
             .on("click", ".filter-by-active", function () { Subscriptions.filterByStatus(this, "ACTIVE") })
             .on("click", ".filter-by-paused", function () { Subscriptions.filterByStatus(this, "PAUSED") })
             .on("click", ".filter-by-canceled", function () { Subscriptions.filterByStatus(this, "CANCELED")})
-            .on("click", ".btn-page", function () {
-                const newPage = parseInt($(this).attr("data-page"));
-                Subscriptions.getSubscriptions(newPage, this.size);
-            })
+            .on("click", ".btn-page", function () { Subscriptions.onUpdatePage(this); })
             .on("click", ".prev-page", () => this.getSubscriptions(Math.abs(this.page - 1), this.size))
             .on("click", ".next-page", () => this.getSubscriptions(this.page + 1, this.size))
             .on("click", "ul.items li.item", function () { Subscriptions.launchProductModal($(this).attr("data-sku")) })
@@ -32,23 +29,45 @@ const Subscriptions = {
             .on("click", ".cancel-subscription", function () { Subscriptions.launchConfirmationModal($(this).attr("id"),"subscription-cancel-confirmation","CANCELED") })
             .on("click", ".activate-subscription", function () { Subscriptions.launchConfirmationModal($(this).attr("id"),"subscription-activate-confirmation","ACTIVE") })
             .on("click", ".confirm-cancel-suscription,.confirm-activate-suscription", () => this.confirmUpdateSubscription())
-            .on("click", ".login-with-email", function (event) {
-                event.preventDefault();
-
-                const email = $("input#email_inline").val();
-                const password = $("input#password").val();
-                
-                if (!email || !password) return M.toast({html: 'Completa los datos!'});
-                if (!Subscriptions.isAValidEmail(email)) return M.toast({ html: 'Email no válido!' });
-                Subscriptions.loginWithEmailAndCode(email, password);
-            })
+            .on("click", ".login-with-email", function (event) { Subscriptions.onLogin(event); })
             
     },
 
+    /** 
+     * Setter
+     */
     setEmail (email) {
         this.email = email;
     },
 
+    onUpdatePage (context) {
+        const newPage = parseInt($(context).attr("data-page"));
+        Subscriptions.getSubscriptions(newPage, Subscriptions.size);
+    },
+
+    /**
+     * @desc This function is triggered when the user clicks over Login button
+     * @param {DOMClickEvent} event 
+     * @returns 
+     */
+    onLogin(event) {
+        event.preventDefault();
+
+        const email = $("input#email_inline").val();
+        const password = $("input#password").val();
+        
+        if (!email || !password) return M.toast({html: 'Completa los datos!'});
+        if (!Subscriptions.isAValidEmail(email)) return M.toast({ html: 'Email no válido!' });
+        if(!Subscriptions.isAPuppisEmail(email)) return M.toast({ html: 'Solo válido para correos Puppis!' });
+
+        Subscriptions.loginWithEmailAndCode(email, password);
+    },
+
+    /**
+     * @desc It performs a login action by requesting an API
+     * @param {String} email 
+     * @param {String} code 
+     */
     loginWithEmailAndCode (email, code) {
         if (!email || !code) return console.error("Missed data");
 
@@ -67,6 +86,9 @@ const Subscriptions = {
             .catch(err => console.error(err));
     },
 
+    /**
+     * @desc It initializes the modal Materializecss
+     */
     initModal () {
         document.addEventListener('DOMContentLoaded', function() {
             var elems = document.querySelectorAll('.modal');
@@ -76,6 +98,11 @@ const Subscriptions = {
         $('.modal').modal();
     },
 
+    /**
+     * @desc    It checks if an string is a real email
+     * @param {String} email 
+     * @returns 
+     */
     isAValidEmail (email) {
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
             return true;
@@ -83,6 +110,32 @@ const Subscriptions = {
         return false;
     },
 
+    /**
+     * @desc    This function verifies if the user to attempt to login is from Puppis
+     * @param {String} email 
+     * @returns {Boolean} True if email belongs to Puppis, false otherwise
+     */
+    isAPuppisEmail (email) {
+        
+        if (!email) return false;
+        if (email.indexOf("@") == -1 ) return false;
+        const emailSplitted = email.split("@");
+        if (emailSplitted.length != 2) return false;
+
+        if (/puppis/ig.test(emailSplitted[1])) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+
+    /**
+     * @desc It search on the server all the possible emails containing the searching keyword
+     * @param {DOMEvent} event 
+     * @param {String} email 
+     * @returns 
+     */
     searchByEmail(event, email) {
         event.preventDefault(); // prevent reloads before the code
 
@@ -91,7 +144,7 @@ const Subscriptions = {
         
         this.renderLoadingSubscriptions();
         let reqSubscriptions = [];
-        const SIZE = 50;
+        const SIZE = 50; // Number of retreived rows
 
         for (let i=1;i<this.limitSearch;i++) {
             reqSubscriptions.push(this._getSubscriptions(i, SIZE));
@@ -119,6 +172,13 @@ const Subscriptions = {
 
     },
 
+    /**
+     * @desc It POSTs the user info to perform a login
+     * @private
+     * @param {String} email 
+     * @param {String} code 
+     * @returns {Promise}
+     */
     _postLogin(email, code) {
         const options = {
             method: "POST",
@@ -127,19 +187,38 @@ const Subscriptions = {
             },
             body: JSON.stringify({ email, code })
         }
-        return fetch(`http://127.0.0.1:5000/api/subscriptions/login`, options);
+        return fetch(`/api/subscriptions/login`, options);
     },
 
+    /**
+     * @desc    It retreives the list of subscriptions by page & size
+     * @private
+     * @param {Number} page 
+     * @param {Number} size 
+     * @returns {Promise}
+     */
     _getSubscriptions (page=1, size=this.size) {
         if (page == 0) page = 1;
-        return fetch(`http://127.0.0.1:5000/api/subscriptions?page=${page}&size=${size}`).then(res => res.json())
+        return fetch(`/api/subscriptions?page=${page}&size=${size}`).then(res => res.json())
     },
 
+    /**
+     * @desc    It retreives info of one only Subscription by its ID
+     * @param {String} id 
+     * @returns {Promise} 
+     */
     _getSubscriptionById (id) {
         if (!id) return console.log("There is not ID");
-        return fetch(`http://127.0.0.1:5000/api/subscriptions/${id}`).then(res => res.json())
+        return fetch(`/api/subscriptions/${id}`).then(res => res.json())
     },
 
+    /**
+     * @desc   It updates on Vtex the subscription
+     *          - It must have the subscription id and status
+     * @private
+     * @param {Object} subscription
+     * @returns 
+     */
     _patchSubscriptions(subscription) {
         if(!subscription) return console.error("There is not subscription to update");
     
@@ -150,14 +229,24 @@ const Subscriptions = {
             },
             body: JSON.stringify(subscription)
         }
-        return fetch(`http://127.0.0.1:5000/api/subscriptions/${subscription.id}`, options).then(res => res.json())
+        return fetch(`/api/subscriptions/${subscription.id}`, options).then(res => res.json())
     },
 
+    /**
+     * @desc    It retreives the product info by its SKUID
+     * @param {String} skuId 
+     * @returns 
+     */
     _getProductInfoBySku (skuId) {
         if (!skuId) return;
-        return fetch(`http://127.0.0.1:5000/api/catalog/${skuId}`).then(res => res.json());
+        return fetch(`/api/catalog/${skuId}`).then(res => res.json());
     },
 
+    /**
+     * @desc    It gets all the subscriptions and update the UI
+     * @param {1} page 
+     * @param {1} size 
+     */
     getSubscriptions (page=1, size=this.size) {
         this.page = page;
 
@@ -168,6 +257,12 @@ const Subscriptions = {
             .catch(err => console.error(err));
     },
 
+    /**
+     * @desc    It Launch a confirmation modal before updating the subscription
+     * @param {String} subscriptionId 
+     * @param {String} elementId 
+     * @param {String} action - New Status for the subscription, default:CANCELED
+     */
     launchConfirmationModal(subscriptionId, elementId, action="CANCELED") {
         this._getSubscriptionById(subscriptionId)
             .then(subscription => {
@@ -187,6 +282,12 @@ const Subscriptions = {
             })
     },
 
+    /**
+     * @desc    Content for Confirmation Modal
+     * @param {Object} subscription 
+     * @param {String} elementId 
+     * @returns 
+     */
     renderConfirmationModal(subscription, elementId="subscription-cancel-confirmation" ) {
         if (!subscription) return;
         $("#"+elementId+" .progress").hide();
@@ -198,6 +299,9 @@ const Subscriptions = {
         $("#"+ elementId +" button.confirm-delete-suscription").attr("data-email", customerEmail );
     },
 
+    /**
+     * @desc    Content after the user confirms that he wants to update the subscription
+     */
     confirmUpdateSubscription() {
         if (!this.subscription || Object.keys(this.subscription).length == 0) return console.log("There is not subscription to update.");
         
@@ -221,6 +325,10 @@ const Subscriptions = {
             });
     },
 
+    /**
+     * @desc    It launch the Product Popup
+     * @param {String} skuId 
+     */
     launchProductModal(skuId) {
         this._getProductInfoBySku(skuId)
             .then(data => !Object.keys(data).some(d => d == "success") ? data : null)
@@ -228,6 +336,11 @@ const Subscriptions = {
             .catch(err => console.error(err));
     },
 
+    /**
+     * @desc    It renders the product info in the Popup
+     * @param {Object} product 
+     * @returns 
+     */
     renderProductModal(product) {
 
         if (!product) {
@@ -252,6 +365,12 @@ const Subscriptions = {
         $("#product-detail-modal .modal-footer a.go-to-product").attr("href", "https://www.puppis.com.co/" + product.LinkId + "/p");
     },
 
+
+    /**
+     * @desc It renders all the rows of subscriptions like in a table view
+     * @param {Array} subscriptions 
+     * @returns 
+     */
     updateRenderSubscriptions (subscriptions) {
         if (!subscriptions || subscriptions.length == 0) return this.renderEmptySubscriptions();
         let _layoutSubscriptions = "";
@@ -289,11 +408,17 @@ const Subscriptions = {
         $(".subscriptions-list").html(_layoutSubscriptions);
     },
 
+    /**
+     * @desc    Helper
+     */
     renderLoadingSubscriptions () {
         const _loadingLayoyt = `<div class="progress yellow lighten-5"><div class="indeterminate yellow darken-2"></div></div>`;
         $(".subscriptions-list").html(_loadingLayoyt);
     },
 
+    /**
+     * @desc    Helper when there is not Subscriptions either in a search or by default
+     */
     renderEmptySubscriptions () {
         
         const _layoutEmpty = `
@@ -316,6 +441,12 @@ const Subscriptions = {
         $(".subscriptions-list").html(_layoutEmpty);
     },
 
+
+    /**
+     * @desc    It renders the pagination
+     * @param {Number} from - default:1
+     * @param {*} to - default:5
+     */
     renderPagination (from=1, to=5) {
         
         let _paginationLayout = "";
@@ -332,6 +463,12 @@ const Subscriptions = {
         $(".pagination").html(_paginationLayout);
     },
 
+    /**
+     * @desc    It is triggered on click over the menu -> It filters all the subscriptions based on the status
+     *          - It does NOT perform a search, it only filters what it finds on the current page
+     * @param {Ctx} context 
+     * @param {String} status 
+     */
     filterByStatus(context,status) {
         
         $("ul.menu-subscriptions li").each(function () { 
